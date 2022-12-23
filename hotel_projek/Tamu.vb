948186ad3ec1f42001b736242e19b2e5d4e50067
rosnawati30 <rosnawati1412@gmail.com>
2022-12-23 12:46:47 +0700

    Menambahkan class DataTamu di form tamu

--- hotel_projek/Tamu.vb
+++ hotel_projek/Tamu.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Tamu
+    Public Shared DataTamu As DataTamu
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        DataTamu = New DataTamu
+    End Sub
 End Class
