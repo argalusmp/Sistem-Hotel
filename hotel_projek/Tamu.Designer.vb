d45c566222150051adfe35056adabc40cdd8d06c
rosnawati30 <rosnawati1412@gmail.com>
2022-12-23 09:25:25 +0700

    Menambahkan data grid view dan button di form tamu

---
+++ hotel_projek/Tamu.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Tamu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridViewTamu = New System.Windows.Forms.DataGridView()
+        Me.BtnTambah = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        CType(Me.DataGridViewTamu, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridViewTamu
+        '
+        Me.DataGridViewTamu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewTamu.Location = New System.Drawing.Point(23, 29)
+        Me.DataGridViewTamu.Name = "DataGridViewTamu"
+        Me.DataGridViewTamu.RowHeadersWidth = 51
+        Me.DataGridViewTamu.RowTemplate.Height = 24
+        Me.DataGridViewTamu.Size = New System.Drawing.Size(750, 369)
+        Me.DataGridViewTamu.TabIndex = 0
+        '
+        'BtnTambah
+        '
+        Me.BtnTambah.Location = New System.Drawing.Point(682, 422)
+        Me.BtnTambah.Name = "BtnTambah"
+        Me.BtnTambah.Size = New System.Drawing.Size(91, 32)
+        Me.BtnTambah.TabIndex = 1
+        Me.BtnTambah.Text = "Tambah"
+        Me.BtnTambah.UseVisualStyleBackColor = True
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.Location = New System.Drawing.Point(574, 422)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(91, 32)
+        Me.BtnEdit.TabIndex = 2
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = True
+        '
+        'BtnHapus
+        '
+        Me.BtnHapus.Location = New System.Drawing.Point(461, 422)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(91, 32)
+        Me.BtnHapus.TabIndex = 3
+        Me.BtnHapus.Text = "Hapus"
+        Me.BtnHapus.UseVisualStyleBackColor = True
+        '
+        'Tamu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 498)
+        Me.Controls.Add(Me.BtnHapus)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnTambah)
+        Me.Controls.Add(Me.DataGridViewTamu)
+        Me.Name = "Tamu"
+        Me.Text = "Tamu"
+        CType(Me.DataGridViewTamu, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridViewTamu As DataGridView
+    Friend WithEvents BtnTambah As Button
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents BtnHapus As Button
+End Class
