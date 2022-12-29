adc84bc9d936de0e57da5e4ff5cf13f3925889bc
argalusmp <argalus03@gmail.com>
2022-12-29 23:24:10 +0700

    menambahkan function GetDataBookingDatabase untuk ditampilkan di datagridview

--- hotel_projek/BookingBook.vb
+++ hotel_projek/BookingBook.vb
@@ -22,15 +22,18 @@ Public Class BookingBook
             + "password=" + password + ";" + "database =" + database
         dbConn.Open()
         sqlCommand.Connection = dbConn
-        sqlCommand.CommandText = "SELECT id_booking AS 'ID Booking', 
-                                  id_tamu AS 'ID Tamu', 
-                                  id_kamar AS 'ID Kamar', 
-                                  check_in AS 'Check-In', 
-                                  total_bayar AS 'Price',
-                                  status AS 'Status',
-                                  tamu.nama AS 'Nama'
+        sqlCommand.CommandText = "SELECT booking_kamar.id_booking AS 'ID Booking', 
+                                  booking_kamar.id_tamu AS 'ID Tamu', 
+                                  tamu.nama AS 'Nama',
+                                  booking_kamar.id_kamar AS 'ID Kamar', 
+                                  kamar.nama_kamar AS 'Nama Kamar',
+                                  booking_kamar.check_in AS 'Check-In', 
+                                  booking_kamar.check_out AS 'Check-Out', 
+                                  booking_kamar.total_bayar AS 'Price',
+                                  booking_kamar.status AS 'Status'
                                   FROM booking_kamar
-                                  INNER JOIN tamu ON booking_kamar.id_tamu = tamu.id_tamu"
+                                  INNER JOIN tamu ON booking_kamar.id_tamu = tamu.id_tamu
+                                  INNER JOIN kamar ON booking_kamar.id_kamar = kamar.id_kamar"
 
 
         sqlRead = sqlCommand.ExecuteReader
