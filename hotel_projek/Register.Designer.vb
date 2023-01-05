a86451e227c39e2433dfd651580009b9d326fc07
rhadityahw <rhaditya.hadi.wibowo.tik21@mhsw.pnj.ac.id>
2023-01-05 16:41:41 +0700

    push login register

---
+++ hotel_projek/Register.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Register
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(316, 214)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(261, 22)
+        Me.txtPassword.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(188, 217)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 16)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Password"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(316, 147)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(261, 22)
+        Me.txtEmail.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(188, 150)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 16)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Email"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(316, 86)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(261, 22)
+        Me.txtUsername.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(188, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Username"
+        '
+        'btnRegister
+        '
+        Me.btnRegister.Location = New System.Drawing.Point(502, 288)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(75, 23)
+        Me.btnRegister.TabIndex = 12
+        Me.btnRegister.Text = "Register"
+        Me.btnRegister.UseVisualStyleBackColor = True
+        '
+        'Register
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Register"
+        Me.Text = "Register"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnRegister As Button
+End Class
